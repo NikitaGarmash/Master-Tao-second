97b67cd1898fb64668510fe99ebb752862457aff
Nikita Garmash <nikita.shlapunov71@gmail.com>
2023-07-21 10:42:59 +0300

    f

---
+++ assets/js/custom.js
@@ -0,0 +1,117 @@
+$( document ).ready(function() {
+    $('.first-screen-slider').slick({
+        dots: true,
+        speed: 1000,
+        appendArrows: ".arrows",
+
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                arrows: false
+                }
+            },
+        ]
+    });
+
+    Fancybox.bind(' [data-fancybox="gallery"]', {
+
+    })
+});
+
+
+document.getElementById('fileInput').onchange = function () {
+    //short-name
+    document.getElementById('file-name').innerHTML = this.files[0].name;
+    //long-name
+    //document.getElementById('file-name').innerHTML = this.value;
+}
+
+
+function CheckboxContainerPadding() {
+    const checkButton1 = document.querySelector('.check-button');
+    const checkButton2 = document.querySelectorAll('.check-button');
+
+    checkButton1.classList.add('.test1')
+
+    checkButton2.forEach(item => {
+        item.closest('[class*="col"]').classList.add('padding-top');
+    })
+}
+
+if (document.querySelector('.calculation-form')) {
+    CheckboxContainerPadding()
+} else {
+    null
+}
+
+
+
+// Mobile Only Slider
+
+mobileOnlySlider(".advantages-slider", true, false, 992);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} 
+
+// Mobile Only Slider
+
+$('.slider-template').slick({
+  dots: false,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+       
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
